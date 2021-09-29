f5cc287bdc6d3e3e5c01c798b4ec54939f420169
SeanEdwards2021 <SeanE301292@yahoo.com>
2021-09-29 02:28:15 +0100

    Added and styled Tesla Logo.

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function header() {
   return (
-    <div>
-      header
-    </div>
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="Tesla Logo" />
+      </a>
+      <Menu>
+
+      </Menu>
+    </Container>
   )
 }
 
 export default header
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`
+
+const Menu = styled.div`
+
+`
