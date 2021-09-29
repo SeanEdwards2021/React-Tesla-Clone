c7c987c645eea56fdebc88f2b930674cb6afd680
SeanEdwards2021 <SeanE301292@yahoo.com>
2021-09-29 01:53:29 +0100

    Added props (parameters) for remaining 3 section component calls.

--- src/components/home.js
+++ src/components/home.js
@@ -8,18 +8,30 @@ function home() {
       <Section
         title="Model S"
         description="Order Online for Touchless Delivery"
-        backgroundImg="model-s.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing Inventory"
+        backgroundImg="model-s.jpg"
       />
       <Section
-        
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-y.jpg"
       />
       <Section
-        
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-3.jpg"
       />
       <Section
-      
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-x.jpg"
       />
     </Container>
   )
